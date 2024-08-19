f59bdd3ed1935d8812fd382438387c0abe761cf7
Hussain Yusuf <114601628+uy-scuti100@users.noreply.github.com>
2024-08-19 19:24:29 +0100

    again

--- src/constants/ProjectLinks.js
+++ src/constants/ProjectLinks.js
@@ -1,77 +1,4 @@
 export const links = [
-<<<<<<< HEAD
-   {
-      label: "Nexus",
-      href: "https://react-nexus-nine.vercel.app/",
-      year: "Y. 2023",
-      concept: "C. Nexus Blog app",
-      type: "T. Side Proj",
-   },
-   {
-      label: "Colorado Parents for Quality education",
-      href: "https://colorado-parents-for-quality-education.vercel.app/",
-      year: "Y. 2023",
-      concept: "C. CPQE",
-      type: "T. Volunteered Proj",
-   },
-   {
-      label: "Leap Of Faith",
-      href: "https://leaps-of-faith.vercel.app/",
-      year: "Y. 2023",
-      concept: "C. Leap Of Faith",
-      type: "T. Side Proj",
-   },
-   {
-      label: "Bank Hotel",
-      href: "https://hotel-flame-pi.vercel.app/",
-      year: "Y. 2023",
-      concept: "C. Bank Hotel",
-      type: "T. Side Proj",
-   },
-   {
-      label: "Finstreak",
-      href: "https://finstreak.vercel.app/",
-      year: "Y. 2023",
-      concept: "C. Finstreak",
-      type: "T. Side Proj",
-   },
-   {
-      label: "()siris",
-      href: "https://shoppe-omega.vercel.app/",
-      year: "Y. 2023",
-      concept: "C. Shoppe",
-      type: "T. Side Proj",
-   },
-   {
-      label: "Scuti Travel Website",
-      href: "https://travels-rust.vercel.app/",
-      year: "Y. 2023",
-      concept: "C. Travel Site",
-      type: "T. Side Proj",
-   },
-   {
-      label: "A-lab Skin Products",
-      href: "https://skin-mu.vercel.app/",
-      year: "Y. 2023",
-      concept: "C. A-Lab",
-      type: "T. Side Proj",
-   },
-   {
-      label: "Pet Haven Veterinary",
-      href: "https://pethaven.vercel.app/",
-      year: "Y. 2023",
-      concept: "C. PetHaven",
-      type: "T. Side Proj",
-   },
-   {
-      label: "Avion E-Comerce",
-      href: "https://furniture-six-beta.vercel.app/",
-      year: "Y. 2023",
-      concept: "C. Avion",
-      type: "Client Proj",
-   },
-
-=======
 	{
 		label: "Bank Hotel",
 		href: "https://hotel-flame-pi.vercel.app/",
@@ -106,5 +33,4 @@ export const links = [
 		concept: "E-commerce",
 		state: "",
 	},
->>>>>>> 47a0d82 (new change)
 ];
